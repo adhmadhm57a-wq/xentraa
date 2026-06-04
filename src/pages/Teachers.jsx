69db1a3def715178c4ae2900110185777d2680e7
adhmadhm57a-wq <adhmadhm57a@gmail.com>
2026-06-04 13:36:49 +0300

    Teachers.jsx

--- src/pages/Teachers.jsx
+++ src/pages/Teachers.jsx
@@ -1,18 +1,71 @@
+import { useState, useEffect } from "react";
+
 export default function Teachers() {
 
-  const teachers = [
-    "Adham Ahmed",
-    "Mahmoud",
-    "Karim"
-  ];
+  const [teacherName, setTeacherName] = useState("");
+
+  const [teachers, setTeachers] = useState([]);
+
+  useEffect(() => {
+
+    const savedTeachers =
+      JSON.parse(localStorage.getItem("teachers")) || [];
+
+    setTeachers(savedTeachers);
+
+  }, []);
+
+  const addTeacher = () => {
+
+    const admin =
+      localStorage.getItem("admin");
+
+    if (!admin) {
+
+      alert("الأدمن فقط");
+
+      return;
+
+    }
+
+    const updatedTeachers = [
+      ...teachers,
+      teacherName
+    ];
+
+    setTeachers(updatedTeachers);
+
+    localStorage.setItem(
+      "teachers",
+      JSON.stringify(updatedTeachers)
+    );
+
+    alert("تم إضافة المدرس");
+
+  };
 
   return (
 
     <div className="container">
 
       <h1>
-        المدرسين
+        إدارة المدرسين
       </h1>
+
+      <div className="card">
+
+        <input
+          placeholder="اسم المدرس"
+          onChange={(e) =>
+            setTeacherName(e.target.value)
+          }
+        />
+
+        <button onClick={addTeacher}>
+          إضافة مدرس
+        </button>
+
+      </div>
 
       {
         teachers.map((teacher, index) => (
@@ -22,10 +75,6 @@ export default function Teachers() {
             <h2>
               {teacher}
             </h2>
-
-            <p>
-              مدرس برمجة
-            </p>
 
           </div>
 
