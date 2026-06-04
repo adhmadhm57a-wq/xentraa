b5bfa3572a1fb63447f77d431f685346a4517a3c
adhmadhm57a-wq <adhmadhm57a@gmail.com>
2026-06-04 16:28:16 +0300

    Payment.jsx

---
+++ src/pages/Payment.jsx
@@ -0,0 +1,30 @@
+export default function Payment() {
+
+  return (
+    <div className="container">
+
+      <h1>الدفع</h1>
+
+      <div className="card">
+
+        <h3>فودافون كاش</h3>
+
+        <p>
+          +201002322734
+        </p>
+
+      </div>
+
+      <div className="card">
+
+        <h3>InstaPay</h3>
+
+        <p>
+          domaelmnwofy@gmail.com
+        </p>
+
+      </div>
+
+    </div>
+  );
+}
