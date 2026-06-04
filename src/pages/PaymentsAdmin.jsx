25cf627de2950842b2aa10f5598b7882d0f3351f
adhmadhm57a-wq <adhmadhm57a@gmail.com>
2026-06-04 16:33:06 +0300

    PaymentsAdmin.jsx

---
+++ src/pages/PaymentsAdmin.jsx
@@ -0,0 +1,20 @@
+export default function PaymentsAdmin() {
+
+  return (
+    <div className="container">
+
+      <h1>مراجعة الاشتراكات</h1>
+
+      <div className="card">
+
+        <h3>طلبات الدفع</h3>
+
+        <p>
+          لا توجد طلبات حالياً
+        </p>
+
+      </div>
+
+    </div>
+  );
+}
